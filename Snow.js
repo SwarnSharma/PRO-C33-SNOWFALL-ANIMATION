0bd67a9e5b5e07447b7be34d1d3e2d2e01d0f090
SwarnSharma <79393690+SwarnSharma@users.noreply.github.com>
2021-06-10 16:22:15 +0530

    Add files via upload

---
+++ Snow.js
@@ -0,0 +1,27 @@
+class Snow{
+    constructor(x,y){
+        var options = {
+        }
+    this.image= loadImage("snow.png");
+    this.body = Bodies.circle(x,y,40,40,20,options);
+    
+    this.radius = 5;
+    this.lifetime=200;
+    World.add (world,this.body);
+    }
+    
+    reset(){
+        if(this.body.position.y > height){
+          Matter.Body.setPosition(this.body,{x:random(0,1400),y:random(0,50)});
+        }
+    }
+    display(){
+        push();
+        
+        var pos = this.body.position;
+    
+        imageMode(CENTER);
+        image(this.image,pos.x,pos.y,this.radius,this.radius);
+    pop();
+    }
+}
